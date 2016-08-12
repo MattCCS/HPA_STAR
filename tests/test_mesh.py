3cf26a08928be8be7dd72d4e32227104bfa8d54e
Matthew Cotton <matt@thecottons.com>
2016-08-11 23:51:19 -0400

    Refactor mesh test into unittest

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -1,61 +1,69 @@
 
-# standard
 import time
+import unittest
 
-# custom
-from HPA_STAR import mesh
+from hpastar import mesh
 
-####################################
 
-m = mesh.MeshLeaf((10, 10), (0, 0), {})
-m.make_open()
-BLOCKED = [
-    (1, 7), (1, 8), (2, 7), (2, 8), (3, 7), (3, 8),
+class TestStringMethods(unittest.TestCase):
 
-    (3, 4), (3, 5), (4, 1), (4, 2),
-    (4, 3), (4, 4), (4, 5), (4, 6),
-    (4, 7), (4, 8), (5, 1), (5, 2),
-    (5, 3), (5, 4), (5, 5), (5, 6),
-    (5, 7), (5, 8), (6, 2), (6, 3),
-    (6, 4), (6, 5), (6, 6), (6, 7),
-    (7, 3), (7, 4), (7, 5)
-]  # try toggling (5, 3)  ;)
+    @classmethod
+    def setUp(self):
+        self.m = mesh.MeshLeaf((10, 10), (0, 0), {})
+        self.m.make_open()
+        BLOCKED = [
+            (1, 7), (1, 8), (2, 7), (2, 8), (3, 7), (3, 8),
 
-for each in BLOCKED:
-    m.add_blocked(each)
+            (3, 4), (3, 5), (4, 1), (4, 2),
+            (4, 3), (4, 4), (4, 5), (4, 6),
+            (4, 7), (4, 8), (5, 1), (5, 2),
+            (5, 3), (5, 4), (5, 5), (5, 6),
+            (5, 7), (5, 8), (6, 2), (6, 3),
+            (6, 4), (6, 5), (6, 6), (6, 7),
+            (7, 3), (7, 4), (7, 5)
+        ]  # try toggling (5, 3)  ;)
 
-print m.graph
+        for each in BLOCKED:
+            self.m.add_blocked(each)
 
-print m.contains((0, 0))
-print m.contains((3, 7))
-print m.contains((9, 9))
-print m.contains((10, 10))
-print m.contains((3, 17))
-print m.contains((-5, 0))
+    def test_graph(self):
+        self.assertIsNotNone(self.m.graph)
 
-t0 = time.time()
-out = m.path((2, 4), (8, 5))
-t1 = time.time()
-print t1 - t0
-print
-pr.print_stats()
+    def test_contains(self):
 
-if not out:
-    print "No path!"
-else:
-    print list(out)
+        self.assertTrue(self.m.contains((0, 0)))
+        self.assertTrue(self.m.contains((3, 7)))
+        self.assertTrue(self.m.contains((9, 9)))
+        self.assertFalse(self.m.contains((10, 10)))
+        self.assertFalse(self.m.contains((3, 17)))
+        self.assertFalse(self.m.contains((-5, 0)))
 
-print
-print m.groups
-print m.landlocked_groups
-print m.open_groups
-t0 = time.time()
-m.calculate_local_groups()
-t1 = time.time()
-print t1 - t0
-print
-print m.groups
-print m.landlocked_groups
-print m.open_groups
+    def test_path(self):
+        t0 = time.time()
+        out = self.m.path((2, 4), (8, 5))
+        t1 = time.time()
+        delta = t1 - t0
 
-print m.groups[0] is m.open_groups[0]
+        self.assertIsNotNone(out)
+        self.assertLess(delta, 0.001)
+
+    def test_groups(self):
+        self.assertDictEqual(self.m.groups, {})
+        self.assertDictEqual(self.m.landlocked_groups, {})
+        self.assertDictEqual(self.m.open_groups, {})
+
+        t0 = time.time()
+        self.m.calculate_local_groups()
+        t1 = time.time()
+        delta = t1 - t0
+        self.assertLess(delta, 0.001)
+
+        groups = {0: self.m.navigable()}
+        self.assertDictEqual(self.m.groups, groups)
+        self.assertDictEqual(self.m.landlocked_groups, {})
+        self.assertDictEqual(self.m.open_groups, groups)
+
+        self.assertIs(self.m.groups[0], self.m.open_groups[0])
+
+if __name__ == '__main__':
+    unittest.main()
